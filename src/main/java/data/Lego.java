477092b2d8e096d06d0e329dc908fca073b71876
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-04-30 10:41:50 +0300

    updating lego.java, legoservice.java and index.html

--- src/main/java/data/Lego.java
+++ src/main/java/data/Lego.java
@@ -11,43 +11,43 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="lego")
 public class Lego {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	private int run;
-	private int speed;
-	private int turn;
-	private Timestamp aika=new Timestamp(System.currentTimeMillis());
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private int run;
+    private int speed;
+    private int turn;
+    private Timestamp aika=new Timestamp(System.currentTimeMillis());
 
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getRun() {
-		return run;
-	}
-	public void setRun(int run) {
-		this.run = run;
-	}
-	public int getSpeed() {
-		return speed;
-	}
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-	public int getTurn() {
-		return turn;
-	}
-	public void setTurn(int turn) {
-		this.turn = turn;
-	}
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getRun() {
+        return run;
+    }
+    public void setRun(int run) {
+        this.run = run;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getTurn() {
+        return turn;
+    }
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
 
-	public Timestamp getAika() {
-		return aika;
-	}
-	public void setAika(Timestamp aika) {
-		this.aika = aika;
-	}
+    public Timestamp getAika() {
+        return aika;
+    }
+    public void setAika(Timestamp aika) {
+        this.aika = aika;
+    }
 }
--- src/main/java/services/LegoService.java
+++ src/main/java/services/LegoService.java
@@ -15,42 +15,40 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Produces;
 import data.*;
 
-
-
 @Path("/lego")
 public class LegoService {
-	EntityManagerFactory emf=Persistence.createEntityManagerFactory("lego");	
-	
-	@Path("/getlego")
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getLego() {
-		return "Lego service Legorest2!";
-	}
-	
-	
-	@Path("/setvalues")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Lego setValues(Lego lego) {
-	    EntityManager em=emf.createEntityManager();
-	    em.getTransaction().begin();
-	    em.persist(lego);
-	    em.getTransaction().commit();		
-		return lego;
-	}
-	@SuppressWarnings("unchecked")
-	@Path("/getvalues")
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getValues() {
-	    EntityManager em=emf.createEntityManager();
-	    em.getTransaction().begin();
-		Query q=em.createQuery("select s from Lego s order by s.id desc").setMaxResults(1);
-		List<Lego> list=q.getResultList();
-		em.getTransaction().commit();
-		Lego lego=list.get(0);
-		return lego.getId()+"#"+lego.getRun()+"#"+lego.getSpeed()+"#"+lego.getTurn();
-	}
+    EntityManagerFactory emf=Persistence.createEntityManagerFactory("lego");    
+    
+    @Path("/getlego")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getLego() {
+        return "Lego service Legorest2!";
+    }
+    
+    @Path("/setvalues")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Lego setValues(Lego lego) {
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(lego);
+        em.getTransaction().commit();       
+        return lego;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Path("/getvalues")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getValues() {
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q=em.createQuery("select s from Lego s order by s.id desc").setMaxResults(1);
+        List<Lego> list=q.getResultList();
+        em.getTransaction().commit();
+        Lego lego=list.get(0);
+        return lego.getId()+"#"+lego.getRun()+"#"+lego.getSpeed()+"#"+lego.getTurn();
+    }
 }
