4f66de9e760635ccb8f01f68cce7d94f1bb37dc0
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-05-04 13:00:39 +0300

    setting up the files to run the lego

--- src/main/java/data/Lego.java
+++ src/main/java/data/Lego.java
@@ -1,7 +1,5 @@
 package data;
-
 import java.sql.Timestamp;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,35 +9,43 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="lego")
 public class Lego {
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private int run;
     private int speed;
     private int turn;
-    private Timestamp aika=new Timestamp(System.currentTimeMillis());
+    private Timestamp aika = new Timestamp(System.currentTimeMillis());
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public int getRun() {
         return run;
     }
+
     public void setRun(int run) {
         this.run = run;
     }
+
     public int getSpeed() {
         return speed;
     }
+
     public void setSpeed(int speed) {
         this.speed = speed;
     }
+
     public int getTurn() {
         return turn;
     }
+
     public void setTurn(int turn) {
         this.turn = turn;
     }
@@ -47,6 +53,7 @@ public class Lego {
     public Timestamp getAika() {
         return aika;
     }
+
     public void setAika(Timestamp aika) {
         this.aika = aika;
     }
