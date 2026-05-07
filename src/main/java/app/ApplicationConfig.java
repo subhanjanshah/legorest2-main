1e01b948de407fc7a27230a3f618d1a9803945f8
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-05-07 10:11:40 +0300

    creating applicationconfig.java

---
+++ src/main/java/app/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package app;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+}
